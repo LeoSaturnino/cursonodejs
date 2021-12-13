6e4fe39e08ad231e2a29518a6b3fe09d054c542f
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-13 15:40:26 -0300

    test: singUpController retornar erro se nao tiver o parametro nome

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,19 +1,20 @@
-import { SingUpController } from "./singup";
+import { SingUpController } from './singup'
 
-describe("SignUp Controller", () => {
-  test("Deve retornar erro 400 caso não seja informado o nome ", () => {
-    const sut = new SingUpController();
+describe('SignUp Controller', () => {
+  test('Deve retornar erro 400 caso não seja informado o nome ', () => {
+    const sut = new SingUpController()
     const httpRequest = {
       body: {
         //   name: 'qualquer_nome',
-        email: "qualquer_email@email.com",
-        password: "qualquer_password",
-        password_confirmation: "qualquer_password",
-      },
-    };
+        email: 'qualquer_email@email.com',
+        password: 'qualquer_password',
+        password_confirmation: 'qualquer_password'
+      }
+    }
 
-    const httpResponse = sut.handle(httpRequest);
+    const httpResponse = sut.handle(httpRequest)
 
-    expect(httpResponse.statusCode).toBe(400);
-  });
-});
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param name'))
+  })
+})
