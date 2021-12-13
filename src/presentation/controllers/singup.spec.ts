e5c48debbd4cbee19f9c5ba1e741d7976da5b0d0
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-13 10:55:40 -0300

    test: add test name nao fornecido no signup

---
+++ src/presentation/controllers/singup.spec.ts
@@ -0,0 +1,19 @@
+import { SingUpController } from "./singup";
+
+describe("SignUp Controller", () => {
+  test("Deve retornar erro 400 caso não seja informado o nome ", () => {
+    const sut = new SingUpController();
+    const httpRequest = {
+      body: {
+        //   name: 'qualquer_nome',
+        email: "qualquer_email@email.com",
+        password: "qualquer_password",
+        password_confirmation: "qualquer_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
