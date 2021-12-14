23589353e4bfd059a8b19b396e0841e488a53183
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-14 12:31:08 -0300

    feat: criado validacao do email em signUpController

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,15 +1,26 @@
+import { EmailValidator } from './../protocols/email-validator'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 
 export class SingUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'password_confirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return badRequest(new InvalidParamError('email'))
     }
     return {
       statusCode: 200,
