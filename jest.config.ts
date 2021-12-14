741f2be6bb1aa812fe12a7fbfb4b6b2d0950ace0
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-14 12:30:14 -0300

    refactor: formacao e identacao nos arquivos de config

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,9 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest",
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
