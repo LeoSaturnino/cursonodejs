81a6e5d11a90e39f8ff2d51da57113fe140b5652
Leo Saturnino <leonardosaturnino10@gmail.com>
2021-12-14 15:10:18 -0300

    feat: retorna erro 500 caso tenha algum problema no validator

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,3 +1,4 @@
+import { ServerError } from './../errors/server-error'
 import { EmailValidator } from './../protocols/email-validator'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
@@ -13,18 +14,25 @@ export class SingUpController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'password_confirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'password_confirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return {
-      statusCode: 200,
-      body: {}
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
