ceb52117a41ae79b6e7818101bf62b7d4591cd47
Leo Saturnino <leonardosaturnino10@gmail.com>
2022-01-17 18:22:47 -0300

    test: adicionado validator ao email validator adapter

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,24 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
 
 describe('Email Validator Adapter', () => {
   test('Deve retornar falso se o validator retorna falso', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@email.com')
     expect(isValid).toBe(false)
+  })
+})
+describe('Email Validator Adapter', () => {
+  test('Deve retornar true se o validator retorna true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
   })
 })
