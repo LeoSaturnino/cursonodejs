66b82dafa95edae171e1c8132a23a4b1a2120cea
Leo Saturnino <leonardosaturnino10@gmail.com>
2022-01-18 18:37:56 -0300

    feat: criado o db add account e o db-add-account-protocols

---
+++ src/data/usescases/add-account/db-add-account.ts
@@ -0,0 +1,14 @@
+import { AddAccountModel, AddAccount, Encrypter } from './db-add-account-protocols'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add(account: AddAccountModel): Promise<any> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise((resolve) => resolve(null))
+  }
+}
