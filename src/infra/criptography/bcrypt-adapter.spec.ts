b8014286cd70e0ed16c399df28a919b86e097503
Leo Saturnino <leonardosaturnino10@gmail.com>
2022-01-20 19:12:07 -0300

    refactor: movido o bcrypt adapter para um sut

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -7,18 +7,22 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt)
+  return sut
+}
+
 describe('Bcrypt Adapter', () => {
   test('Garantir que o Bcrypt seja chamado com os valores corretos', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Garantir que o Bcrypt retorne o hash quando sucesso', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
